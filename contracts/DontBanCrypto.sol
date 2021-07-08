81e7bf70c186e8964e945ac9571c97e70600fbbe
Kamal Dev <kamal.dev9339@gmail.com>
2021-07-08 23:07:16 +0530

    Transfer Tokens

--- contracts/DontBanCrypto.sol
+++ contracts/DontBanCrypto.sol
@@ -5,10 +5,24 @@ contract DontBanCrypto {
     string public symbol = "DBC";
     string public version = "Dont Ban Crypto Coin v0.1";
     uint256 public totalSupply;
-    mapping (address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+    }
+
 }
--- test/DontBanCrypto.js
+++ test/DontBanCrypto.js
@@ -33,4 +33,61 @@ contract('DontBanCoin', function(accounts) {
             'Allocates initialSupply to Super User Account');
         });
     });
+/*
+    it('Transfers token to another user', function() {
+        return DontBanCrypto.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1],99999999999999); //Here transfer doesn't triggers a transaction
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.toString().indexOf('revert') >= 0,
+            'Error message must contain revert');
+            return tokenInstance.transfer(accounts[1], 250000,              //Here transfer triggers a transaction
+            {from:accounts[0]});
+        }).then(function(receipt) {
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 250000,
+            'Send amount to the receiver');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 49750000,
+            "Deduct amount from the sender account.");
+        });
+    });
+*/
+    it("Transfers token to another user", function () {
+        return DontBanCrypto.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 9999999999999999);
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message, "Error message must contain revert");
+            return tokenInstance.transfer.call(accounts[1], 250000,
+                {from: accounts[0]});
+        }).then(function (success) {
+          assert(success.toString(), true, "Returned true");
+          return tokenInstance.transfer(accounts[1], 250000,
+              {from: accounts[0]});
+        }).then(function (receipt) {
+            assert.equal(receipt.logs.length, 1, "Triggered an event");
+            assert.equal(receipt.logs[0].event, "Transfer",
+            'Should be the "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, accounts[0],
+                "Logs the account the tokens are transferred from");
+            assert.equal(receipt.logs[0].args._to, accounts[1],
+                "Logs the account the tokens are transferred to");
+            assert.equal(receipt.logs[0].args._value, 250000,
+                "Logs the transfer amount");
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function (reciept) {
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), 250000,
+            "Amount added to the receiving account");
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), 49750000,
+            "Amount deducted from the sender account");
+        });
+    });
+
 });
